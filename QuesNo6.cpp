ab1b361e0b8c0a68810456ef3dbb0b2d72daae56
Asad-aqeel <144030369+Asad-aqeel@users.noreply.github.com>
2023-09-21 08:38:05 -0700

    Update QuesNo6.cpp

--- QuesNo6.cpp
+++ QuesNo6.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 int main() {
     for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cout << "* ";
-        }
-        cout << endl;
+        cout << "* * * * * * * * " << endl;
     }
 
     return 0;
